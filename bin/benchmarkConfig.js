21907c91b347a4e101b006acbd4f551704e078ac
Cosimo Streppone <cosimo@streppone.it>
2013-01-15 21:51:09 +0100

    Better .gitignore and MANIFEST.skip

---
+++ bin/benchmarkConfig.js
@@ -0,0 +1,13 @@
+{
+  "graphitePort": 2003,
+  "graphiteHost": "graphite.local",
+  "host" : "0.0.0.0",
+  "port": 8125,
+  "mgmt_address" : "0.0.0.0",
+  "mgmt_port": 8126,
+  "backends": [ "Graphite", "Console" ],
+  "log" : {
+    "backend" : "stdout",
+    "level" : "LOG_WARN",
+  }
+}
